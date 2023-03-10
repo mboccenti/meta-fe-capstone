a999f04769c5b6c76551c3f9bc0e508986e621ad
Marco Boccenti <marco.boccenti@gmail.com>
2023-03-10 04:18:44 +0100

    clean up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import BookingForm from "./components/BookingForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders the BookingForm", () => {
+  render(<BookingForm />);
+  const submitBtn = screen.getByText("Make Reservation");
+  expect(submitBtn).toBeInTheDocument();
+});
+ 
+test("Updates the time correctly", () => {
+  render(<BookingForm />);
+  const dateSelector = screen.getByLabelText(/Choose date/);
+  fireEvent.change(dateSelector, { target: { value: "2023-02-05" } });
+  const timeDropDown = screen.getByLabelText(/Choose time/);
+  fireEvent.change(timeDropDown, { target: { value: "17:00" } });
+  expect(timeDropDown.value).toEqual("17:00");
 });
